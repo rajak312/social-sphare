63a0e6d77a1227d34a056550db717f9bf356eb9b
Lalit Rajak <lalitrajak312@gmail.com>
2024-12-15 01:45:54 +0530

    debugging the variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import LogIn from "./components/Auth/LogIn";
 
 const App = () => {
+  console.log("ENVIRONMENT: ", import.meta.env.ENV);
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="w-[392px] h-full border bg-red-50">
         <LogIn />
+        <p>Domain: {import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}</p>
+        <p>ProjectId: {import.meta.env.VITE_FIREBASE_PROJECT_ID}</p>
       </div>
     </div>
   );
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIRE_BASE_API_KEY,
@@ -11,4 +10,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
